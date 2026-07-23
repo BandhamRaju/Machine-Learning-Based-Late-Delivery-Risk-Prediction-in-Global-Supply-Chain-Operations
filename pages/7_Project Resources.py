5987471862aaa1135752cb6c5b69c86f1d2d9120
Raju Bandham <Bandhamraju2@gmail.com>
2026-07-23 10:38:14 +0530

    Update 7_Project Resources.py

--- pages/7_Project Resources.py
+++ pages/7_Project Resources.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pandas as pd
 import os
 
-
+import gdown
 #=========================================================
 # PAGE CONFIGURATION
 #=========================================================
